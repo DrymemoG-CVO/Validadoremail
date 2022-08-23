7774a0151744ad9f7d3e2ed06b30efe74d95fe82
DrymemoG-CVO <104378190+DrymemoG-CVO@users.noreply.github.com>
2022-08-23 09:48:13 -0600

    Primer Commit
    Cree el validador de correos

---
+++ regex/regex/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim regexPattern As String = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$"
+        Dim emailRegEx As System.Text.RegularExpressions.Regex = New System.Text.RegularExpressions.Regex(regexPattern)
+        Dim result As Boolean = emailRegEx.IsMatch(TextBox1.Text)
+        If Not result Then
+            MessageBox.Show("Ingrese un correo vàlido")
+        End If
+        If result Then
+            MessageBox.Show("Correo vàlido")
+        End If
+    End Sub
+End Class
